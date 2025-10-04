269eb8f9434126e2a31574d21fc11c00158767b6
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2025-10-04 18:07:56 +0000

    correção final

---
+++ managementsystem/src/main/java/com/project/jujucabomboniermanegementsystem/managementsystem/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.project.jujucabomboniermanegementsystem.managementsystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "redirect:/index.html";  // redireciona para o arquivo estático
+    }
+}
