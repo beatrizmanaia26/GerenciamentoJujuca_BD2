5be793ee9ca69028a0c7ef76b5cd1f035076451a
rafadias008 <123601507+rafadias008@users.noreply.github.com>
2025-11-01 17:13:44 +0000

    adição do historico

--- managementsystem/src/main/java/com/project/jujucabomboniermanegementsystem/managementsystem/ManagementsystemApplication.java
+++ managementsystem/src/main/java/com/project/jujucabomboniermanegementsystem/managementsystem/ManagementsystemApplication.java
@@ -1,3 +1,4 @@
+
 package com.project.jujucabomboniermanegementsystem.managementsystem;
 
 import org.springframework.boot.SpringApplication;
--- managementsystem/src/main/java/com/project/jujucabomboniermanegementsystem/managementsystem/repository/HistoryRepository.java
+++ managementsystem/src/main/java/com/project/jujucabomboniermanegementsystem/managementsystem/repository/HistoryRepository.java
@@ -1,15 +1,14 @@
 package com.project.jujucabomboniermanegementsystem.managementsystem.repository;
 
 import com.project.jujucabomboniermanegementsystem.managementsystem.models.HistoryModel;
-import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface HistoryRepository extends CassandraRepository<HistoryModel, UUID> {
-
-    @AllowFiltering
-    List<HistoryModel> findByDate(Date date);
+    List<HistoryModel> findByDate(LocalDate date);
 }
